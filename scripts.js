16f007f58922904c4b3a23ed4dadc603118a5809
izabela_01 <euizabellaa@gmail.com>
2026-03-11 18:31:42 -0400

    Finalizado

--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
-// Script.js
-document.getElementById("button").addEventListener("click", function() {
-    document.getElementById("demo").innerHTML = "Hello, JavaScript"
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href⁼"#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAtribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
+
+
+// Product hover animations
+const productCards = document.querySelectorAll('.product-card');
+productCards.forEach(card => {
+    card.addEventListener('mouseenter', function() {
+        this.classList.add('translateY(-15px)');
+    });
+    card.addEventListener('mouseleave', function() {
+        this.classList.remove('translateY(-15px)');
+    });
+});
+
+
